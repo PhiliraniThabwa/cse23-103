a1b8fbed9b69fbfa9caf2662b12d5bc95757bfda
PhiliraniThabwa <159142622+PhiliraniThabwa@users.noreply.github.com>
2024-05-22 09:44:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('header nav ul li a');
 
-    links.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            const url = this.getAttribute('href');
-            loadPage(url);
-        });
-    });
-
-    function loadPage(url) {
-        const content = document.getElementById('content');
-
-        // Fade out the content
-        content.style.opacity = '0';
-        setTimeout(function () {
-            // Load new page content
-            fetch(url)
-                .then(response => response.text())
-                .then(html => {
-                    // Insert new page content
-                    content.innerHTML = html;
-                    // Fade in the new content
-                    content.style.opacity = '1';
-                })
-                .catch(error => console.error('Error loading page:', error));
-        }, 500); // Adjust the delay as needed for your transition
-    }
-});
-
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
